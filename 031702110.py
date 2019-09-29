8bd5c6caf6822d3d613cd6282d5bd5e2adabff43
cxxxyy <55096671+cxxxyy@users.noreply.github.com>
2019-09-29 20:18:35 +0800

    Update 031702110.py

--- 031702110.py
+++ 031702110.py
@@ -1,15 +1,15 @@
-#-*- coding:UTF_8 -*-
 import re
 import json
 
+
 city_0=['石家庄','秦皇岛','张家口','葫芦岛','哈尔滨','齐齐哈尔','牡丹江','佳木斯' ,'双鸭山','七台河','连云港','马鞍山','景德镇','平顶山','三门峡','驻马店','张家界','攀枝花','六盘水','嘉峪关','呼和浩特','鄂尔多斯','呼伦贝尔','巴彦淖尔','乌兰察布','防城港','日喀则','石嘴山','乌鲁木齐','克拉玛依','吐鲁番']
 person={
-    "姓名":"",
-    "手机":"",
-    "地址":[],
+    '姓名':'',
+    '手机':'',
+    '地址':[],
     }
 
-def getph(message): 
+def getph(message):
     a=re.findall(r'\d{11}',message)
     number=''.join(a)
     return number
@@ -25,6 +25,7 @@ def divide(i,address):
     (b,q)=a.split(' ',1)
     z=b+i
     return z
+
 
 
 def getaddress_1(address):
@@ -47,7 +48,40 @@ def getaddress_1(address):
         address=re.sub(a,'',address)
         
     else:
-        if address[0:3]=='黑龙江':
+        if '内蒙古' in address:
+            a='内蒙古自治区'
+            address=re.sub('内蒙古','',address)
+            
+        elif '新疆维吾尔' in address:
+            a='新疆维吾尔自治区'
+            address=re.sub('新疆维吾尔','',address)
+            
+        elif '新疆' in address:
+            a='新疆维吾尔自治区'
+            address=re.sub('新疆','',address)
+                           
+        elif '广西壮族' in address:
+            a='广西壮族自治区'
+            address=re.sub('广西壮族','',address)
+            
+        elif '广西' in address:
+            a='广西壮族自治区'
+            address=re.sub('广西','',address)
+            
+        elif '宁夏回族' in address:
+            a='宁夏回族古自治区'
+            address=re.sub('宁夏回族','',address)
+            
+        elif '宁夏' in address:
+            a='宁夏回族古自治区'
+            address=re.sub('宁夏','',address)
+            
+        elif '西藏' in address:
+            a='西藏古自治区'
+            address=re.sub('西藏','',address)
+            
+            
+        elif address[0:3]=='黑龙江':
             a='黑龙江省'
             address=re.sub('黑龙江','',address)
             
@@ -117,7 +151,7 @@ def getaddress(message):
     
     address=re.sub(',','',address)
     address=address.replace('.','',1)
-    print(address)
+    
     if flag==1:
         [a,b,c,d,address]=getaddress_1(address)
         e=address
@@ -140,11 +174,15 @@ def getaddress(message):
             address=re.sub(f,'',address)
 
         g=address
-        return [a,b,c,d,e,f,g]
-
+    
+    return [a,b,c,d,e,f,g]
+        
+     
+        
 message=input()
-person["姓名"]=getname(message)
-person["手机"]=getph(message)
-person["地址"].extend(getaddress(message))
+person['姓名']=getname(message)
+person['手机']=getph(message)
+person['地址'].extend(getaddress(message))
 Json=json.dumps(person,ensure_ascii=False)
-print(Json)  
+print(Json)
+
